0916c478329d95e814abff2d6782a0b6f0874782
Luis de Leon <luyzdeleon@gmail.com>
2020-02-10 14:15:20 -0400

    Updated MsgSend to match current Pocket Core spec

--- go/lib/pokt-network/posmint/x/bank/msgs.go
+++ go/lib/pokt-network/posmint/x/bank/msgs.go
@@ -8,7 +8,7 @@ var _ sdk.Msg = MsgSend{}
 
 // MsgSend - high level transaction of the coin module
 type MsgSend struct {
-	FromAddress sdk.Address `json:"from_address" yaml:"from_address"`
-	ToAddress   sdk.Address `json:"to_address" yaml:"to_address"`
-	Amount      sdk.Coins   `json:"amount" yaml:"amount"`
+	FromAddress sdk.Address
+	ToAddress   sdk.Address
+	Amount      sdk.Int
 }
