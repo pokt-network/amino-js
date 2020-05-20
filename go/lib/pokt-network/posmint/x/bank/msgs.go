c8bf50f6220db7cad576a37d1e62609018376cea
Luis de Leon <luyzdeleon@gmail.com>
2020-05-20 00:47:34 -0400

    Added all necessary updates for pocket core rc-0.4.0

--- go/lib/pokt-network/posmint/x/bank/msgs.go
+++ go/lib/pokt-network/posmint/x/bank/msgs.go
@@ -8,7 +8,7 @@ var _ sdk.Msg = MsgSend{}
 
 // MsgSend - high level transaction of the coin module
 type MsgSend struct {
-	FromAddress sdk.Address
-	ToAddress   sdk.Address
-	Amount      sdk.Int
+	FromAddress sdk.Address `json:"from_address"`
+	ToAddress   sdk.Address `json:"to_address"`
+	Amount      sdk.Int     `json:"amount"`
 }
